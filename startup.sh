63f259f13dd0b18e658ec58c1eabf107458d3282
brucewayne91 <iambatman19940222@163.com>
2021-12-09 16:00:36 +0800

    fix

--- startup.sh
+++ startup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-if [ -d "/data" ]; then
+if [ -e /data/init/init.lock ]; then
 	echo "跳过初始化"
 else
 	echo "安装初始化开始"
-	mkdir -p /data
+	mkdir -p /data/init && touch /data/init/init.lock && mkdir -p /data/db
 	username=${MONGODB_USERNAME:-admin}
 	password=${MONGODB_PASSWORD:-admin}
 	database=${MONGODB_DBNAME:-admin}
+
 	if [ ! -z "$MONGODB_DBNAME" ]
 	then
     	userrole=${MONGODB_ROLE:-dbOwner}
@@ -15,11 +16,11 @@ else
 	fi
 
 	# 首次运行创建用户
-	/usr/bin/mongod --dbpath /data --nojournal &
+	/usr/bin/mongod --dbpath /data/db --nojournal &
 	while ! nc -vz localhost 27017; do sleep 1; done
 
 	mongo ${database} --eval "db.createUser({ user: '${username}', pwd: '${password}', roles: [ { role: '${userrole}', db: '${database}' } ] });"
-	/usr/bin/mongod --dbpath /data --shutdown
+	/usr/bin/mongod --dbpath /data/db --shutdown
 fi
 
-exec /usr/bin/mongod --dbpath /data --auth --bind_ip_all
+exec /usr/bin/mongod --dbpath /data/db --auth --bind_ip_all
